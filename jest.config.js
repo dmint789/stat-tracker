637cc6ad1281a39c44f5ba3568b1bdfca364f2df
Deni <cube327@tuta.io>
2023-04-10 12:00:35 +0200

    Update jest config for React Navigator

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+
+  // From https://callstack.github.io/react-native-testing-library/docs/react-navigation
+  //setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
+  ],
 };
